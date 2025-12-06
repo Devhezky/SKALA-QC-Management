7194611e335a1129ef19025e7601aace3838f9a1
Devhezky <150407928+Devhezky@users.noreply.github.com>
2025-12-07 00:15:05 +0700

    Change skalaUrl to use production URL

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -131,7 +131,7 @@ export function LoginForm({
                 const left = window.screen.width / 2 - width / 2;
                 const top = window.screen.height / 2 - height / 2;
 
-                const skalaUrl = 'http://localhost:8888/skala-new/index.php';
+                const skalaUrl = 'https://skala.narapatistudio.com/index.php';
                 const ssoUrl = `${skalaUrl}/admin/qc_integration/qc_sso/login?popup=1`;
 
                 window.open(
